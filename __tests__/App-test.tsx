25fdbeebf431291315c6b77184177707c0eedae1
Dalu26 <ikeanyichukwudalu@gmail.com>
2022-07-14 02:36:35 +0100

    Revert "chore: tests"
    This reverts commit 643077a82d19557c5c99b2223928be04954325d1.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,14 +4,14 @@
 
 import 'react-native';
 import React from 'react';
-import { UserProvider } from '../src/context/providers/UserContext';
+import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const app = renderer.create(
-  <UserProvider />
+  <App />
   );
   expect(app).toMatchSnapshot()
 })
